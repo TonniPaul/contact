1ef3b6a881135855d060b70ae339ae1204314e84
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-13 05:12:05 +0100

    minor updates

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,11 @@ export default function About (){
       <main>
          <h3>About</h3>
          <p>
-            I am a frontend developer with a particular interest in making things
-            simple and catchy. I try to keep up with security and 
-            best practices, and am always looking for new things to learn.
+            I am a frontend developer with particular interest in building amazing 
+            websites and user interfaces using html, css, javascript and React. 
+            I try to keep up with web security and best practices, and am always 
+            looking to learn new things.
+
          </p>
       </main>
    )
