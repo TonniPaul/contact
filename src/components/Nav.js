c18985f48ebabbbd63ba1882d27f3b3b6868c827
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-25 23:46:12 +0100

    Added Whatsapp icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,37 +2,43 @@ import './Nav.css'
 
 function Nav (){
    return(
-      <nav>
-         <a href='#root'>
-             <h2 className="logo"><span className='tt'> t</span><span>onni</span>Paul</h2>
-         </a>
-         <ul>
-            <li>
-               <a href="#about">
-                  <div className='icons'>
-                     <i class="fa-solid fa-person"></i>
-                     <p>About</p>
-                  </div>
-               </a>
-            </li>
-            <li>
-               <a href="#contact">
-                  <div className='icons'>
-                  <i class="fa-brands fa-dev " ></i>
-                     <p>Projects</p>
-                  </div>
-               </a>
-            </li>
-            <li>
-               <a href="#contact">
-                  <div className='icons'>
-                     <i class="fa-solid fa-envelope"></i>
-                     <p>Contacts</p>
-                  </div>
-               </a>
-            </li>
-         </ul>
-      </nav>
+      <>
+         <nav>
+            <a href='#root'>
+               <h2 className="logo"><span className='tt'> t</span><span>onni</span>Paul</h2>
+            </a>
+            <ul>
+               <li>
+                  <a href="#about">
+                     <div className='icons'>
+                        <i class="fa-solid fa-person"></i>
+                        <p>About</p>
+                     </div>
+                  </a>
+               </li>
+               <li>
+                  <a href="#contact">
+                     <div className='icons'>
+                     <i class="fa-brands fa-dev " ></i>
+                        <p>Projects</p>
+                     </div>
+                  </a>
+               </li>
+               <li>
+                  <a href="#contact">
+                     <div className='icons'>
+                        <i class="fa-solid fa-envelope"></i>
+                        <p>Contacts</p>
+                     </div>
+                  </a>
+               </li>
+            </ul>
+          
+         </nav>
+         <a href='http://wa.me/2348162325194/' className='whatsapp'>
+               <i class="fa-brands fa-whatsapp"></i>
+            </a>
+         </>
    )
 }
 export default Nav;
