e64b8f3fcb0a797547fc62a264638081a3612e65
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-31 21:13:41 +0100

    Beautified, changed folder name, and hero font

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+
+function Hero(){
+   return (
+      <div className="hero-body">
+         <main>
+            <p>
+            👋 Hey I'm Paul Oluwatoni, a Frontend developer. 
+            I'm currently learning ReactJS.
+            </p>
+         </main>
+      </div>
+   );
+};
+export default Hero;
