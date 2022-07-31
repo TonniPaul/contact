a2e7c6c2a4bba149276c9497170a7bc557a337f4
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-07-31 19:53:45 +0100

    More features and backgorund added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,14 @@
-import email from "../images/email.svg"
-import linkedin from "../images/linkedin.svg"
 
 function Contact(){
    return (
-      <main className="contact">
-         <div className="contact--details">
-            <h3>Paul O. Ariyo-Adeoye</h3>
-            <p>Frontend Developer</p>
-            <a href="http://tonnipaul.live" className="tpl">tonnipaul.live</a>
-            <div className="contact-btn">
-               <a href="mailto:ariyoadeoyepaul@gmail.com" target='blank' className="contact-btn-mail">
-                  <img src={email} alt='m'/>
-                  Email
-               </a>
-               <a href="https://www.linkedin.com/in/tonnipaul/" target="blank" className="contact-btn-lkd">
-                  <img src={linkedin} alt='m'/>
-                  LinkedIn
-               </a>
-            </div>
-         </div>
-      </main>
+      <div className="contact">
+         <main>
+            <p>
+            👋 Hey I'm Paul, a Frontend developer. 
+            I'm currently learning ReactJS.
+            </p>
+         </main>
+      </div>
    );
 };
 export default Contact;
