bb5eb8999709441252a630a0283f9fbb845279be
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-01 16:48:53 +0100

    Edited probgarm

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,8 @@
+import './project.css'
 export default function Projects() {
    return(
-      <main className="int myabout" style={{ 
-         display: "flex", 
-         width:'80vw',
-         height: '20vh',
-         background: "#fff" , 
-         color: '#000',
-         alignItems:'center',
-         justifyContent: 'center',
-         margin:'10px auto 100px'
+      <main className="project" style={{ 
+        
          }}>
          <h1>Under Construction</h1>
         
