1e37759e557c5cda468a60e746372f65a45d2a76
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-02 17:05:23 +0100

    edited 404 page

--- src/Invalidpage.js
+++ src/Invalidpage.js
@@ -2,8 +2,12 @@ import invalidimg from './images/danger.svg'
 export default function Invalidpage(){
    return (
          <div className="invalid myabout">
+            <h4>Error 404</h4>
             <img src={ invalidimg } alt='invalid img' className='invalid--img'/>
-            <h1> this page doesn't exist!!<br/> or might have been deleted</h1>
+            <h1> this page doesn't exist!<br/> or might have been deleted
+            <br/>
+            Go back home
+            </h1>
          </div>
    );
 }
