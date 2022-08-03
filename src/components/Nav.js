db70c9d4a6110d9a7640499ea68607052d7f73ef
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-03 17:31:54 +0100

    Added my cv

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
 import './Nav.css';
+import pdf from '../cv/paulcv.pdf';
 import { Link } from "react-router-dom";
 
 function Nav (){
    return(
       <>
          <nav>
+            <div className='logocv'>
                <h2 className="logo"><span className='tt'> t</span><span>onni</span>Paul</h2>
+               <a href={pdf} target='blank' className='download'> Download CV</a>
+            </div>
             <ul>
                <Link to='/home'>
                   <li>
