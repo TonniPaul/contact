651980167a4fc509b3d4439c4d95547dea8dae1e
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-03 19:48:13 +0100

    moved download from all screen

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-
+import pdf from '../cv/paulcv.pdf';
 function Hero(){
    return (
       <div className="hero-body">
@@ -8,6 +8,7 @@ function Hero(){
             I create Awesome Web Applications.
             </p>
          </main>
+         <a href={pdf} target='blank' className='download'> Download CV</a>
       </div>
    );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,7 +12,6 @@ function Nav (){
             <div className='logocv'>
                <h2 className="logo"><span className='tt'> t</span><span>onni</span>Paul</h2>
                <ReactSwitch/>
-               <a href={pdf} target='blank' className='download'> Download CV</a>
             </div>
             <ul>
                <Link to='/home'>
