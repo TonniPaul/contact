afd5526a700f245f261a9dacd0d09138c75b9368
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-17 00:30:08 +0100

    new color pallete

--- src/components/Toolset.js
+++ src/components/Toolset.js
@@ -7,12 +7,43 @@ function Toolset(){
          <h3>Toolset</h3>
          <hr/>
          <div>
-            <i class="fa-brands fa-html5"></i>
-            <i class="fa-brands fa-css3"></i>
-            <i class="fa-brands fa-bootstrap"></i>
-            <i class="fa-brands fa-js"></i>
-            <i class="fa-brands fa-react"></i>
-            <i class="fa-brands fa-git"></i>
+            <div className='skilldescription'>
+               <i class="fa-brands fa-html5"></i>
+               <p className='skill--text'>
+                  HTML5
+               </p>
+            </div>
+            <div className='skilldescription'>
+               <i class="fa-brands fa-css3"></i>
+               <p className='skill--text'>
+                  CSS3
+               </p>
+            </div>
+            <div className='skilldescription'>
+               <i class="fa-brands fa-bootstrap"></i>
+               <p className='skill--text'>
+                  BOOTSTRAP
+               </p>
+            </div>
+            <div className='skilldescription'>
+               <i class="fa-brands fa-js"></i>
+               <p className='skill--text'>
+                  JAVASCRIPT
+               </p>
+            </div>
+            <div className='skilldescription'>
+               <i class="fa-brands fa-react"></i>
+               <p className='skill--text'>
+                  REACTJS
+               </p>
+            </div>
+            <div className='skilldescription'>
+               <i class="fa-brands fa-git"></i>
+               <p className='skill--text'>
+                  GIT
+               </p>
+            </div>
+
          </div>
       </main>
       </>
