c17bdec4eb63a0499afdf00a3c062e268b81c5c5
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-16 16:16:22 +0100

    Made the dark mode toggle work

--- src/components/Toolset.js
+++ src/components/Toolset.js
@@ -9,6 +9,7 @@ function Toolset(){
          <div>
             <i class="fa-brands fa-html5"></i>
             <i class="fa-brands fa-css3"></i>
+            <i class="fa-brands fa-bootstrap"></i>
             <i class="fa-brands fa-js"></i>
             <i class="fa-brands fa-react"></i>
             <i class="fa-brands fa-git"></i>
