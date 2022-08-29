6970a6fdec1d661f1df1c57e150c987f08bf6b05
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-08-29 19:22:54 +0100

    linked one of the projects properly

--- src/components/projectObject.js
+++ src/components/projectObject.js
@@ -12,7 +12,7 @@ export const projectData =[
       image: './images/memelarge.png',
       description:'A web Application that generates random Meme Images and allows you to input your own Header and Footer caption ',
       stacks:'HTML, CSS, JAVASCRIPT and REACT',
-      liveSite: 'https://https://tonnimeme.netlify.app/',
+      liveSite: 'https://tonnimeme.vercel.app',
       github: 'https://github.com/TonniPaul/meme'
    }
 ]
