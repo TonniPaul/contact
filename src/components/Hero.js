b743b83c461a4be98f3b5008d1536f443c949f5b
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-11-03 16:00:54 +0100

    Updated CV

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,22 +5,35 @@ import { NavLink } from 'react-router-dom';
 function Hero()
 {
    return (
-      <div className='herooo'>
-         <div className="hero-body">
-            <main className='heromain'>
-               <ImagePaul/>
-               <div className='hero--text'>
-                  <p className='name--text'>Paul Oluwatoni Ariyo-Adeoye</p>
-                  <small className='name--text--small'>Frontend Developer</small>
-               </div>
-               <div className='hero-button'>
-                  <p><NavLink to='/contact'className='action--btn'> Hire Me</NavLink></p>
-                  <p><a href={pdf} target='blank' className='action--btn'> Resume</a></p>
-               </div>
-            </main>
-            
-         </div>
-      </div>
+     <div className="herooo">
+       <div className="hero-body">
+         <main className="heromain">
+           <ImagePaul />
+           <div className="hero--text">
+             <p className="name--text">Paul Oluwatoni Ariyo-Adeoye</p>
+             <small className="name--text--small">Frontend Developer</small>
+           </div>
+           <div className="hero-button">
+             <p>
+               <NavLink to="/contact" className="action--btn">
+                 {" "}
+                 Hire Me
+               </NavLink>
+             </p>
+             <p>
+               <a
+                 href="https://1drv.ms/b/s!AqOoOpxoiQY4gRrIPX5ofiVuX5RY?e=n5e2Pl"
+                 target="blank"
+                 className="action--btn"
+               >
+                 {" "}
+                 Resume
+               </a>
+             </p>
+           </div>
+         </main>
+       </div>
+     </div>
    );
 };
 export default Hero;
